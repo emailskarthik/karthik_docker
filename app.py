c13a7de4c782f29051e77f02ef9d80c2e501d453
Karthikeyan <109635007+emailskarthik@users.noreply.github.com>
2022-10-18 23:41:08 +0530

    updated

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello-World from karthik'
+    return 'Hello-World from karthikeyan'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=6000)
